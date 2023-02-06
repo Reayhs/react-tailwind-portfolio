8ddd9d4f3237e611bbb07503dc825b594ce603cd
kubade220 <32162454+kubade220@users.noreply.github.com>
2023-02-06 13:37:26 +0000

    Pending changes exported from your codespace

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <>
     <div className="footer bg-[#171717]">
       <div className="wrap flex justify-between items-center px-12 text-white font-bold py-6 tl:px-4 tl:py-4 ">
-        <h1 className='text-3xl'>Reayhs</h1>
+        <h1 className='text-3xl'><Kubade220></Kubade220></h1>
         <div className='flex gap-4 cursor-pointer '>
         <i className="fa-brands fa-twitter border-2 border-white p-2 rounded-[100%]"></i>
         <i className="fa-brands fa-instagram  border-2 border-white p-2 rounded-[100%]"></i>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         <div className="btn py-3">
           <button  className=' border-2 border-[#b0aaaa] font-bold text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-4 rounded-2xl'>Welcome to my Portfolio</button>
         </div>
-        <h1 className='text-white text-5xl font-bold'>Hi! I'm Burak</h1>
+        <h1 className='text-white text-5xl font-bold'>Hi! <Kubade></Kubade></h1>
         <p className='text-white py-4 max-w-lg '>Lorem ipsum dolor sit amet consectetur  adipisicing elit. In qui impedit  sunt, est molestiae ullam. Saepe voluptatum ducimus adipisci pariatur. adipisicing elit.  In qui impedit ipsum dolor sit amet consectetur</p>
         <button className='  text-white text-2xl'>Let's Connect <i className="fa-solid fa-arrow-right text-lg  p-[2px] "></i> </button>
       </div>
