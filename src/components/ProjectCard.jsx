e4b78b37d90c6d73cf46c71eb6d4a4343a37c861
burak <Reayhs1@gmail.com>
2023-02-28 15:14:23 +0300

    style changed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function ProjectCard({item}) {
   return (
     <>
-      <div className="img-box flex justify-center relative overflow-hidden">
-              <img src={item.img} alt="" />
+      <div className="img-box w-[450px] mx-auto flex justify-center relative overflow-hidden rounded-3xl">
+              <img src={item.img} alt="" className='w-full' />
               <div className="p absolute top-[-100%] left-0 h-full w-full text-center bg-[#b004b0b7] p-4 pt-20">
                 <h1 className="font-bold text-3xl">{item.title}</h1>
                 <p className="font-bold">{item.description}</p>
